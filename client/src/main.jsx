45c59e9de1d58ec49bf6d55f4b615b02078e8e10
acoder25 <ashita.pahwa18@gmail.com>
2025-11-01 12:52:36 +0530

    landing page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+function Register(){
+  return(
+    <>
+    </>
+  );
+
+};
+export default Register;
